4bf937f8adf9b1bb1f9d4cebcd07da307e63494f
Dmitry <dima9km@gmail.com>
2020-07-28 17:38:35 +0300

    loader added at the JobsFragment

--- app/src/main/java/com/dima/jobs/ui/screens/jobs/JobsFragment.java
+++ app/src/main/java/com/dima/jobs/ui/screens/jobs/JobsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -24,8 +25,9 @@ public class JobsFragment extends Fragment {
 
     JobsDownloader jobsDownloader = new JobsDownloader(new Listener() {
         @Override
-        public void showLoader(boolean setVisibility) {
-            //TODO
+        public void showLoader(boolean visible) {
+            ProgressBar progressBar = getActivity().findViewById(R.id.pbJobs);
+            progressBar.setVisibility(visible ? View.VISIBLE : View.GONE);
         }
 
         @Override
--- app/src/main/java/com/dima/jobs/utils/Listener.java
+++ app/src/main/java/com/dima/jobs/utils/Listener.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface Listener {
 
-    void showLoader(boolean setVisibility);
+    void showLoader(boolean visible);
 
     void onGetData(List<Job> jobs);
 
